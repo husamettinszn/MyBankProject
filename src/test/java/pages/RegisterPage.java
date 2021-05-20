0dbd6b6463652c937ae2991b05d13ddfa1c975bd
husamettinszn <husamettinszn@gmail.com>
2021-05-20 12:22:59 +0200

    US_002

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,10 +1,19 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 
 public class RegisterPage {
-    public RegisterPage(){
-        PageFactory.initElements(WebDriver.d);
+    public RegisterPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+
     }
+    @FindBy(xpath = "(//a[@class='dropdown-toggle nav-link'])[2]")
+    public WebElement dropdownMenu;
+
 }
+
---
+++ target/test-classes/features/1.UsrStory.feature
@@ -0,0 +1,4 @@
+Feature:
+  Scenario:
+    When User click start menu
+    
