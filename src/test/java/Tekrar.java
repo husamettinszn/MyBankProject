6768de90bdf7028e815002dc1b9066f2c1cea948
husamettinszn <husamettinszn@gmail.com>
2021-09-13 17:02:51 +0200

    A

--- src/test/java/Tekrar.java
+++ src/test/java/Tekrar.java
@@ -3,12 +3,40 @@ import java.util.Scanner;
 public class Tekrar {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Sayi giriniz");
-        int ilkSayi= scan.nextInt();
-        System.out.println("Bir sayi daha giriniz");
-        int ikinciSayi = scan.nextInt();
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Sayi giriniz");
+            int ilkSayi= scan.nextInt();
+            System.out.println("Bir sayi daha giriniz");
+            int ikinciSayi = scan.nextInt();
 
 
-    }
+            System.out.println("Islem seciniz");
+            String islem = scan.next();
+            int sonuc = 0;
+
+
+
+            if (islem.equals("+")) {
+                sonuc = ilkSayi + ikinciSayi;
+
+            }
+            if (islem.equals("-")) {
+                if (ilkSayi >= ikinciSayi) {
+                    sonuc = ilkSayi - ikinciSayi;
+
+                } else {
+                    sonuc = ikinciSayi - ilkSayi;
+                }
+            }
+
+            if (islem.equals("*")) {
+                sonuc = ilkSayi * ikinciSayi;
+            }
+            if (islem.equals("/")) {
+                sonuc = ilkSayi / ikinciSayi;
+            }
+            System.out.println("Sonuc = " + sonuc);
+        }while (true);
+        }
 }
